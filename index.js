f9e4d6e4c242d05c43cf29bfb835f8b543bf6cf8
solange25 <66730552+solange25@users.noreply.github.com>
2020-08-17 13:41:33 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+<<<<<<< HEAD
+console.log('It works');
+$(document).ready(function () {
+  $('.submit').click(function (event) {
+    console.log('clicked button');
+    let name = $('.name').val();
+    let email = $('.email').val();
+    let message = $('.message').val();
+    let statusElm = $('.status');
+    statusElm.empty('');
+    if (name.length >= 10) {
+      statusElm.append('<div>Name is valid</div>');
+    } else {
+      event.preventDefault();
+      statusElm.append('<div>Name is not valid</div>');
+    }
+    if (email.length > 5 && email.includes('@') && email.includes('.')) {
+      statusElm.append('<div>Email is valid</div>');
+    } else {
+      event.preventDefault();
+      statusElm.append('<div>Email is not valid</div>');
+    }
+    if (message.length >= 10) {
+      statusElm.append('<div>Message is  valid</div>');
+    } else {
+      event.preventDefault();
+      statusElm.append('<div>Message is not valid</div>');
+    }
+  });
+});
+=======
 console.log('It works');
 $(document).ready(function () {
   $('.submit').click(function (event) {
@@ -27,3 +58,4 @@ $(document).ready(function () {
     }
   });
 });
+>>>>>>> 421c15504cb301f223792ff245d177a6102c97fb
